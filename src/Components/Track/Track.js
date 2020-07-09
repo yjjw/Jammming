0f750d71e897a99e6c25dbfd04bdc4ba464a2364
Jingwei Ye <yjjw97@gmail.com>
2020-07-09 16:59:00 -0700

    Static Components

---
+++ src/Components/Track/Track.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import './TrackList.css';
+
+class Track extends React.Component {
+    renderAction() {
+        if (this.props.isRemoval) {
+            return <button className='Track-action'>-</button>
+        } else {
+            return <button className='Track-action'>+</button>
+        }
+    }
+    render() {
+        return (
+            <div className="Track">
+                <div className="Track-information">
+                    <h3>track name</h3>
+                    <p>track artist and track album will go here</p>
+                </div>
+                {this.renderAction()}
+            </div>
+        );
+    }
+}
+export default Track;
---
+++ src/Components/TrackList/TrackList.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './TrackList.css';
+import '../Track/Track';
+
+class TrackList extends React.Component {
+    render() {
+        return (
+            <div className="TrackList">
+
+            </div>
+        );
+    }
+}
+export default TrackList;
